8e99ae5ce0a92487852147a02de150d52624cdf9
flowerhead <flowerhead@users.noreply.github.com>
2022-09-26 19:05:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
 <?php
 
-require_once "navi.php";
-
-if ($_SERVER['REQUEST_URI'] === '/') {
-    $file = 'home';
-} else {
-    $path = explode( "/", trim($_SERVER['REQUEST_URI'], "/"));
-    $file = $path[0];
-
-    if (isset($path[1])) {
-        $id = $path[1];
-        $_GET['pic'] = $id;
-    }
-}
-
-if (!file_exists($file . '.php')) {
-    header("HTTP/1.0 404 Not Found");
-    echo "not found 404";
-    die;
-}
-
-include($file . '.php');
-
-
+echo <h1>Hallo Welt!</h1>
