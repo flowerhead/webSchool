3b5063965bade3725edc0d089c35e188a5d2ef8b
flowerhead <juli.lillepea@gmail.com>
2022-09-26 21:42:42 +0200

    syntaxFix2

--- gallery.php
+++ gallery.php
@@ -6,8 +6,7 @@ while ($file = readdir($dir)) {
     if (!is_file('pics/' . $file)) {
         continue;
     }
-    $fileName = explode('.', $file)[0];
-    echo '<div style="float:left; margin:10px;"><a href="/detail/' . $fileName . '"><img src="pics/' . $file . '" height="250"></a></div>';
+    echo '<div style="float:left; margin:10px;"><img src="pics/' . $file . '" height="250"></div>';
 }
 
 closedir($dir);
