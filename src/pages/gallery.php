af33615b0d58e9723781edb17cd44217f18a4d8b
flowerhead <juli.lillepea@gmail.com>
2022-09-28 21:34:40 +0200

    thumbnails

--- src/pages/gallery.php
+++ src/pages/gallery.php
@@ -1,14 +1,14 @@
 <?php
 
-$dir = opendir('pics');
+$dir = opendir('pics/thumbnails');
 
 while ($file = readdir($dir)) {
-    if (!is_file('pics/' . $file)) {
+    if (!is_file('pics/thumbnails/' . $file)) {
         continue;
     }
 
     $filename = explode('.', $file)[0];
-    echo '<div style="float:left; margin:10px;"><a href="/details/' . $filename . '"><img src="pics/' . $file . '" height="250"></a></div>';
+    echo '<div style="float:left; margin:10px;"><a href="/details/' . strtoupper($filename) . '"><img src="pics/thumbnails/' . $file . '" height="250"></a></div>';
 }
 
 closedir($dir);
